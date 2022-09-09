97a1a1779d9a8cef888101ec92d60831a89d5b89
dev-yoga <spin_shoppe@icloud.com>
2022-09-09 14:06:28 -0700

    posts

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -17,6 +20,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ('-publish',)
         def __str__(self):
